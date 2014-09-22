f88a7ade43bcb9dfda74412f0e5563f731d71a6e
mikefetick <mikefetick@gmail.com>
2014-09-22 09:39:21 -0700

    program

---
+++ protected/views/customer/update.php
@@ -0,0 +1,21 @@
+<?php
+/* @var $this CustomerController */
+/* @var $model Customer */
+
+$this->breadcrumbs=array(
+	'Customers'=>array('index'),
+	$model->customer_id=>array('view','id'=>$model->customer_id),
+	'Update',
+);
+
+$this->menu=array(
+	array('label'=>'List Customer', 'url'=>array('index')),
+	array('label'=>'Create Customer', 'url'=>array('create')),
+	array('label'=>'View Customer', 'url'=>array('view', 'id'=>$model->customer_id)),
+	array('label'=>'Manage Customer', 'url'=>array('admin')),
+);
+?>
+
+<h1>Update Customer <?php echo $model->customer_id; ?></h1>
+
+<?php $this->renderPartial('_form', array('model'=>$model)); ?>
---
+++ protected/views/grid/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mike
+ * Date: 9/20/14
+ * Time: 5:16 PM
+ */
+?>
+<?php
+    $this->widget('zii.widgets.grid.CGridView', array('dataProvider'=>$dataProvider, ) )
+?>
